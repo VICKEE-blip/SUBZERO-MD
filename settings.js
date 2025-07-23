f388be00b68f8d2f301017b9c2695cddb833c15e
VICKEE-blip <mentorvickee@gmail.com>
2025-07-23 22:45:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;LcFylS5C#x29hyUPm1-iT983r_U7TM4_4UXv-_3Nfr8Yr-uGSdBY", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
